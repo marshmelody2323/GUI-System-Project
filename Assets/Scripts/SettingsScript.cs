26690466c7dddb0e09f3019d21e90996a4819a06
marshmelody2323 <mysticcreeper99@gmail.com>
2021-04-27 20:08:46 +1000

    Update 2
    Added resolutions to the settings menu

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 
 public class SettingsScript : MonoBehaviour
+
+
+    
+
 {
    public void setFullScreen(bool fullscreen)
     {
@@ -13,6 +17,17 @@ public class SettingsScript : MonoBehaviour
     public void setQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    List<int> widths = new List<int>() {568, 960, 1280, 1920};
+    List<int> heights = new List<int>() {329, 540, 800, 1080};
+
+    public void SetScreenSize(int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
     }
 
 }
