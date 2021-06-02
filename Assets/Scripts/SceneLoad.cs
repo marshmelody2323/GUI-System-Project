8892734e021739cc2de8caff5fefcda8a1e22286
Melody Lynn <mysticcreeper99@gmail.com>
2021-06-02 17:15:12 +1000

    Update 3
    just resized gui elements for 1080p screens

---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoad : MonoBehaviour
+{
+    [SerializeField]
+    private Image _progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadLevelAsync(3);
+        while(gameLevel.progress < 1)
+        {
+            _progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
